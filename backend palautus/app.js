533cfa1d35f25f44fb368c19bbab71047b5e32e4
LottaL <32096459+LottaL@users.noreply.github.com>
2018-11-29 18:05:06 +0200

    Update app.js

--- backend palautus/app.js
+++ backend palautus/app.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const session = require('express-session');
-const app = express();
 
 const database = require('./modules/database');
 const resize = require('./modules/resize');
@@ -73,7 +72,7 @@ app.use('/upload', (req, res) => {
 app.use(bodyParser.urlencoded({extended: true}));
 
 const db = require('./modules/database');
-const connection = db.connect();
+const dbconnection = db.connect();
 
 passport.serializeUser((user, done) => {
   console.log('serialize: ' + user);
